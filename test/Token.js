7b49197539bf23b53a525351dccfe6ec71293689
Aboudoc <aboutika.reda@gmail.com>
2023-01-24 04:55:41 +0100

    test

--- test/Token.js
+++ test/Token.js
@@ -12,13 +12,34 @@ describe("Token", function () {
   }
 
   describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { token, owner, otherAccount } = await loadFixture(
-        deployTokenFixture
-      );
+    it("should have minted the owner tokens", async function () {
+      const { token, owner } = await loadFixture(deployTokenFixture);
+
       const balance = await token.balanceOf(owner.address);
 
       assert.equal(balance.toString(), 1000);
+    });
+
+    it("should allow us to send tokens", async () => {
+      const { token, owner, otherAccount } = await loadFixture(
+        deployTokenFixture
+      );
+
+      await token.transfer(otherAccount.address, 999);
+
+      const balanceOwner = await token.balanceOf(owner.address);
+      const balanceOther = await token.balanceOf(otherAccount.address);
+
+      assert.equal(balanceOwner.toString(), 1);
+      assert.equal(balanceOther.toString(), 999);
+    });
+
+    it("it should allow us to do approve/tranferFrom", async () => {
+      const { token, owner, otherAccount } = await loadFixture(
+        deployTokenFixture
+      );
+      const balanceOwner = await token.balanceOf(owner.address);
+      assert.equal(balanceOwner.toString(), 1000);
     });
   });
 });
